d901859e95a3f55f5bf637421cb23174d3469ed8
Anthony <misterantony88@hotmail.fr>
2012-04-03 17:17:21 +0200

    Premiere source

---
+++ src/RoundBouton.java
@@ -0,0 +1,29 @@
+import java.awt.Dimension;
+
+import javax.swing.Icon;
+import javax.swing.JButton;
+
+
+public class RoundBouton extends JButton {
+
+	private static final long serialVersionUID = 7824724817261535225L;
+
+public RoundBouton(Icon icon) {
+    super(icon);
+    setBorderPainted(false);
+    setFocusPainted(false);
+    setContentAreaFilled(false);
+  }
+
+  public boolean contains(int x, int y) {
+    Dimension size = getSize();
+    float x0 = size.width / 2F;
+    float y0 = size.height / 2F;
+ 
+    Icon icon = getIcon();
+    float w = icon.getIconWidth() / 2F;
+    float h = icon.getIconHeight() / 2F;
+ 
+    return (x - x0) * (x - x0) + (y - y0) * (y - y0) <= w * h;
+  }
+}
---
+++ src/Station.java
@@ -0,0 +1,71 @@
+import java.util.HashMap;
+
+
+public class Station {
+	private int tempsarret;
+	private String nom;
+	private boolean incident;
+	private HashMap<Station,Integer> temps_vers_station_voisine;//retourne -1 si il y a un incident sur la voie
+	private Coordonnee coordonnee_station;
+	private int ligne;
+	//1 pour ligne1,2 pour ligne 2,3 pour ligne 3,4 pour ligne 1 et 2, 5 pour ligne 2 et 3, 6 pour ligne 1 et 3
+	
+	public Station(int t,String n,HashMap<Station,Integer> h,Coordonnee c,int l){
+		tempsarret = t;
+		nom = n;
+		temps_vers_station_voisine=h;
+		coordonnee_station = c;
+		ligne=l;
+		incident=false;
+	}
+
+	public int getLigne() {
+		return ligne;
+	}
+
+	public void setLigne(int ligne) {
+		this.ligne = ligne;
+	}
+
+	public int getTempsarret() {
+		return tempsarret;
+	}
+
+	public void setTempsarret(int tempsarret) {
+		this.tempsarret = tempsarret;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public boolean isIncident() {
+		return incident;
+	}
+
+	public void setIncident(boolean incident) {
+		this.incident = incident;
+	}
+
+	public HashMap<Station, Integer> getTemps_vers_station_voisine() {
+		return temps_vers_station_voisine;
+	}
+
+	public void setTemps_vers_station_voisine(
+			HashMap<Station, Integer> tempsVersStationVoisine) {
+		temps_vers_station_voisine = tempsVersStationVoisine;
+	}
+
+	public Coordonnee getCoordonnee_station() {
+		return coordonnee_station;
+	}
+
+	public void setCoordonnee_station(Coordonnee coordonneeStation) {
+		coordonnee_station = coordonneeStation;
+	}
+	
+}
