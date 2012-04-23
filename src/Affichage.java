0b5858cab3e6b4c05415886efbfb078cb635ca3c
Anthony <misterantony88@hotmail.fr>
2012-04-23 11:53:42 +0200

    last affichage

--- src/Affichage.java
+++ src/Affichage.java
@@ -9,8 +9,11 @@ import java.util.ArrayList;
 import java.util.Map.Entry;
 
 import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,6 +21,7 @@ import javax.swing.JMenu;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
@@ -35,11 +39,11 @@ public class Affichage {
 	private boolean positionner=false;;
 	private ArrayList<Station> chemin = new ArrayList<Station>();
 	private ArrayList<ArrayList<Station>> solutions = new ArrayList<ArrayList<Station>>();
-	private Station depart,arrive;
+	private Station depart,arrive,temp1; //temp1 et temp2 les fonctions par ou passer
 	
 	Affichage(AlgoRechercheChemin a){
 		algo = a;
-		jf.add(jp);
+		
 		jf.setSize(new Dimension(900,700));
 		jf.setTitle("Terminal Mobile Metro");
 		jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -47,6 +51,16 @@ public class Affichage {
 		jf.setVisible(true);
 		jf.setResizable(false);
 		jf.setVisible(true);
+		jf.setLayout(new BorderLayout());
+		jf.add(jp);
+		
+		JPanel radio = new JPanel();
+		JRadioButton jradio = new JRadioButton("Chemin le plus rapide");
+		JRadioButton jradio1 = new JRadioButton("Moins de changement de lignes");
+		radio.add(jradio);
+		radio.add(jradio1);
+		jf.add(radio,BorderLayout.NORTH);
+		
 	}
 	
 	public void paint(Graphics g, JButton[] tableaubutton, Station[] lestation){
@@ -124,27 +138,68 @@ public class Affichage {
 		donnerPositionDepart();
 		JOptionPane.showMessageDialog(jp,"Donnez votre position d'arriver");
 		donnerPositionArrive();		
-		algo.Tous_Les_Chemins(chemin, solutions,depart,arrive);
-		ArrayList<Station> cheminplusrapide=algo.cheminPlusRapide(solutions);
-
-		for (Station station : cheminplusrapide) {
-			for (int i = 0; i < lestation.length; i++) {
-				if(lestation[i].getNom().compareTo(station.getNom())==0){
-					setStation(i);
+		int option = JOptionPane.showConfirmDialog(null, "Voulez-vous passez par un endroit précis?", "Station intermédiaire", 
+					JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+		
+		if(option==JOptionPane.OK_OPTION){
+			JOptionPane.showMessageDialog(jp,"Donnez votre position par ou passer");
+			donnerPositionAPasser();
+			algo.Tous_Les_Chemins(chemin, solutions,depart,temp1);
+			ArrayList<Station> cheminplusrapide=algo.cheminPlusRapide(solutions);
+			int premiertemps = algo.tempsPlusRapide(solutions); 
+			for (Station station : cheminplusrapide) {
+				for (int i = 0; i < lestation.length; i++) {
+					if(lestation[i].getNom().compareTo(station.getNom())==0){
+						setStation(i);
+					}
 				}
 			}
-		}
-		if(depart!=arrive){
-			BarreProgression frame = new BarreProgression();
-	        frame.pack();
-	        frame.setVisible(true);
-	        frame.loop();
-	        frame.setVisible(false);
-			int totalsecondes = algo.tempsPlusRapide(solutions); 
-			int secondes = totalsecondes % 60;
-			int minutes = (totalsecondes / 60) % 60;
-			JOptionPane.showMessageDialog(jp,"Voila le chemin à prendre \nTemps estimé : "+minutes+" "+
-						"minutes "+secondes+" secondes \nNombres de changements : "+cheminplusrapide.size());
+			algo.Tous_Les_Chemins(chemin, solutions,temp1,arrive);
+			ArrayList<Station> cheminplusrapide2=algo.cheminPlusRapide(solutions);
+			for (Station station : cheminplusrapide2) {
+				for (int i = 0; i < lestation.length; i++) {
+					if(lestation[i].getNom().compareTo(station.getNom())==0){
+						setStation(i);
+					}
+				}
+			}
+			if(depart!=arrive){
+				BarreProgression frame = new BarreProgression();
+		        frame.pack();
+		        frame.setVisible(true);
+		        frame.loop();
+		        frame.setVisible(false);
+				int totalsecondes = algo.tempsPlusRapide(solutions)+premiertemps; 
+				int secondes = totalsecondes % 60;
+				int minutes = (totalsecondes / 60) % 60;
+				JOptionPane.showMessageDialog(jp,"Voila le chemin à prendre \nTemps estimé : "+minutes+" "+
+							"minutes "+secondes+" secondes \nNombres de changements : "+(cheminplusrapide.size()+cheminplusrapide2.size()));
+			}
+			
+		}else{
+			algo.Tous_Les_Chemins(chemin, solutions,depart,arrive);
+			
+			ArrayList<Station> cheminplusrapide=algo.cheminPlusRapide(solutions);
+	
+			for (Station station : cheminplusrapide) {
+				for (int i = 0; i < lestation.length; i++) {
+					if(lestation[i].getNom().compareTo(station.getNom())==0){
+						setStation(i);
+					}
+				}
+			}
+			if(depart!=arrive){
+				BarreProgression frame = new BarreProgression();
+		        frame.pack();
+		        frame.setVisible(true);
+		        frame.loop();
+		        frame.setVisible(false);
+				int totalsecondes = algo.tempsPlusRapide(solutions); 
+				int secondes = totalsecondes % 60;
+				int minutes = (totalsecondes / 60) % 60;
+				JOptionPane.showMessageDialog(jp,"Voila le chemin à prendre \nTemps estimé : "+minutes+" "+
+							"minutes "+secondes+" secondes \nNombres de changements : "+cheminplusrapide.size());
+			}
 		}
 		
 	}
@@ -159,6 +214,25 @@ public class Affichage {
 				int stationplusproche=algo.proche(e.getX(),e.getY(),lestation);
 				setStation(stationplusproche);
 				depart=lestation[stationplusproche];
+				jp.removeMouseListener(this);
+				positionner=true;
+			}
+		});
+		while(positionner==false);//on attend le clic de la position
+		positionner=false;
+		return positionner;
+	}
+	
+	public boolean donnerPositionAPasser(){
+		jp.addMouseListener(new MouseListener() {
+			public void mouseReleased(MouseEvent e) {}
+			public void mousePressed(MouseEvent e) {}
+			public void mouseExited(MouseEvent e) {}
+			public void mouseEntered(MouseEvent e) {}
+			public void mouseClicked(MouseEvent e) {
+				int stationplusproche=algo.proche(e.getX(),e.getY(),lestation);
+				setStation(stationplusproche);
+				temp1=lestation[stationplusproche];
 				jp.removeMouseListener(this);
 				positionner=true;
 			}
@@ -187,6 +261,7 @@ public class Affichage {
 			}
 		});
 		while(positionner==false);//on attend le clic de la position
+		positionner=false;
 		return positionner;
 	}
 	
